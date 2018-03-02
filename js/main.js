e3165820cc316092e70575e742d2a61cfea5c72c
Christopher Hooks <christopher.hooks89@gmail.com>
2018-03-02 13:08:05 -0600

    Initial Commit
    Added basic HTML layout, some CSS styling, and beginning of slick js
    slideshow

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.auto-play').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+  });
+      

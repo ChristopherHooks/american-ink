0eec563c3614f7b731dbda9e834d74377b85bfc8
Christopher Hooks <christopher.hooks89@gmail.com>
2018-03-10 23:18:30 -0600

    Added images, modified shop info section, moved js to external file.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-// $(document).ready(function(){
-//     $('.auto-play').slick({
-//         // slidesToShow: 3,
-//         // slidesToScroll: 3,
-//         // autoplay: true,
-//         // autoplaySpeed: 2000,
-//     });
-//   });
-      
+$(function() {
+    $(".rslides").responsiveSlides({
+        auto: true,
+        speed: 1000,
+        maxwidth: 500,
+        timeout: 3000
+    });
+});
+$(document).ready(function() {
+    $("#sticker").sticky({
+        topSpacing: 0
+    });
+
+    $("#sticker").on("sticky-start", function() {
+        $("#sticker").css("background-color", "black");
+    });
+
+    $("#sticker").on("sticky-end", function() {
+        $("#sticker").css("background-color", "transparent");
+    });
+});
